4c13d452528a238bbcc813dbea5fdb7ed36cb6e1
hantriseptiania <71365751+hantriseptiania@users.noreply.github.com>
2020-09-30 13:20:56 +0700

    Add files via upload

---
+++ precache-manifest.dfdb6775681cbc41a5bc5c11f01a5a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92f22283647485005cd25039b44b686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c799b3558b29b4feed2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d032ddace490a7d9689a",
+    "url": "/static/js/2.c07254c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c07254c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c799b3558b29b4feed2",
+    "url": "/static/js/main.2afd75cc.chunk.js"
+  },
+  {
+    "revision": "8a1da142a5fb7b296fd1",
+    "url": "/static/js/runtime-main.a1bb2dc5.js"
+  }
+]);
